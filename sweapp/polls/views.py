5ea6743a676e2fd496f9775e7e13e133ebc5d018
XEternalKingX <zoeyac02@gmail.com>
2025-10-26 18:24:21 -0500

    web setup

--- sweapp/polls/views.py
+++ sweapp/polls/views.py
@@ -1,6 +1,28 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 def index(request):
     return HttpResponse("Hello world. You're at the polls index.")
 
 # Create your views here.
+
+# testing out views.. - ash
+# unless its supposed to be (for example)'main/login.html' <- it was lmao
+def login_view(request):
+    return render(request, 'main/login.html')
+
+def feed_view(request):
+    return render(request, 'main/feed.html')
+
+def events_view(request):
+    return render(request, 'main/events.html')
+
+def messages_view(request): 
+    return render(request, 'main/messages.html')
+
+def profile_view(request):
+    return render(request, 'main/profile.html')
+
+def navbar(request): # do we need a view for navbar?
+    return HttpResponse("This is the navbar component.")
--- sweapp/sweapp/settings.py
+++ sweapp/sweapp/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -102,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'CDT'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
